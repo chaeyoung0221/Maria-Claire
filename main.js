f6e0c7fdfb4dbd392d0095836ffb7c899a89c852
chaeyoung0221 <chaeyoung9747@naver.com>
2025-06-20 20:47:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -29,28 +29,28 @@ const itemsData = [
   itemLink: "https://www.samsung.com/sec/",
   itemCopy:
     "무광인덕션<br>프리미엄에<br>트렌드를 더하다",
-  itemImg: "images/Stel uw persoonlijke stijlboek samen.jpg",
+  itemImg: "Stel uw persoonlijke stijlboek samen.jpg",
 },
   {
     itemName: "Augustinus Bader",
     itemCategory: "Beauty",
     itemLink: "https://augustinusbader.com/int/en/?gc_id=9234598288&h_ad_id=715803577901&gad_source=1&gad_campaignid=9234598288&gbraid=0AAAAAC8_8EOnWXFqjjJ8StAO4UzxoPBGw&gclid=CjwKCAjwgb_CBhBMEiwA0p3oODDlUxQRqyadRY_B76UiQ0RPHo98nkWcsz6yaO9lrmQFYts8zpsZhxoC7_EQAvD_BwE",
     itemCopy: "헤어케어의 새로운 기준 <br>TFC8 헤어 리뉴얼 시스템",
-    itemImg: "images/Augustinus Bader The 12 Days of Bader Kit.jpg",
+    itemImg: "Augustinus Bader The 12 Days of Bader Kit.jpg",
   },
   {
     itemName: "MATERASSO",
     itemCategory: "Living",
     itemLink: "https://materasso.eu/",
     itemCopy: "좋은 잠을 찾아서,마테라소",
-    itemImg: "images/Polsterbett Tayla aus Teddy-Bouclé.jpg",
+    itemImg: "Polsterbett Tayla aus Teddy-Bouclé.jpg",
   },
   {
     itemName: "JAKOMO",
     itemCategory: "Living",
     itemLink: "https://jakomosofa.com/",
     itemCopy: "변함없는 1등 소파<br>결국 자코모",
-    itemImg: "images/Carmo Sofa by BoConcept.jpg",
+    itemImg: "Carmo Sofa by BoConcept.jpg",
   },
 ];
 
